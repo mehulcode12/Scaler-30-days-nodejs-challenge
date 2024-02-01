4ce88aa0a88a4d5311cf5816abcc8169f1ca65b2
Mehul Ligade <107620902+mehulcode12@users.noreply.github.com>
2024-02-01 23:45:26 +0530

    Add files via upload

---
+++ Day 1/session.js
@@ -0,0 +1,15 @@
+console.log("Scalar is Awesome")
+
+function sayHello() {
+    console.log("Helllo! I am Mehul")
+}
+console.log()
+let name = 'Mehul'
+console.log(global.name)
+console.log()
+//////////////
+
+const calculator = require('./calculator')
+calculator.addition(4,6)
+calculator.subtraction(4,6)
+
